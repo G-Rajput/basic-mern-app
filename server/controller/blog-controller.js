6ab0661a62a0f616316599fb3de004971a849c64
Govind Rajput <govindrajput9819@gmail.com>
2024-05-02 23:23:11 +0530

    Fetch blogList controller Added

--- server/controller/blog-controller.js
+++ server/controller/blog-controller.js
@@ -5,3 +5,17 @@ import Blog from "../modal/Blog";
 //add a new blog
 //delete a blog
 //update a blog
+
+const fetchListOfBlogs = async (req, res) => {
+  let blogList;
+  try {
+    blogList = await Blog.find();
+  } catch (error) {
+    console.log(error);
+  }
+  if (!blogList) {
+    return res.status(404).json({ message: "No Blogs Found" });
+  }
+
+  return res.status(200).json({ blogList });
+};
